95bd40723064810bcfec713a8ab64f876842f8bb
omar ghanem <ghanemomar002@gmail.com>
2026-07-23 16:12:39 +0300

    Add if-else logic for weird number classification
    Implement if-else logic to determine if a number is weird or not based on specified conditions.

---
+++ If-else.py
@@ -0,0 +1,10 @@
+#HackerRank-If-else
+n = int(input())
+if n % 2 != 0:
+    print("Weird")
+elif n % 2 ==0 and n <= 5 and n >= 2:
+    print("Not Weird")
+elif n % 2 ==0 and n <= 20 and n >=6:
+    print("Weird")
+else:
+    print("Not Weird")
